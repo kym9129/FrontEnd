220c8f97facfec058308bb79ca47bd0e2b80b456
Youngmi Kim <72649415+kym9129@users.noreply.github.com>
2021-03-09 21:41:31 +0900

    Update 실습문제_객체배열.js
    prototype에 중복 메소드 적용

--- js/실습문제_객체배열.js
+++ js/실습문제_객체배열.js
@@ -5,9 +5,9 @@
  * - html5
  * - css3
  * - javascript
- * - getSum
- * - getAvg : 반올림 소수점 첫째자리
- * - toString
+ * - getSum ->prototype으로 올리기
+ * - getAvg : 반올림 소수점 첫째자리  ->prototype으로 올리기
+ * - toString  ->prototype으로 올리기
  */
 
 function Student(name, java, oracle, html5, css3, javascript){
@@ -17,32 +17,63 @@ function Student(name, java, oracle, html5, css3, javascript){
     this.html5 = html5;
     this.css3 = css3;
     this.javascript = javascript;
-    this.getSum = function(){
-        return Number(this.java + this.oracle + this.html5 + this.css3 + this.javascript);
-    };
-    this.getAvg = function(){
-        // var avg = Math.round(this.getSum() / this.length * 100) / 100;
-        var avg = Math.round(this.getSum() / 5 * 100) / 100;
-        // console.log("this.length = " + this.length);
-        return avg;
-    };
-    this.toString = function(){
-        var str = "";
-        for(var key in this){
-            if(key !="toString" && key != "getAvg" && key != "getSum"){
-                str += key + "=" + this[key] + ", ";
-            }
-        }
-        return str;
-    };
+    // this.getSum = function(){
+    //     return Number(this.java + this.oracle + this.html5 + this.css3 + this.javascript);
+    // };
+    // this.getAvg = function(){
+    //     // var avg = Math.round(this.getSum() / this.length * 100) / 100;
+    //     var avg = Math.round(this.getSum() / 5 * 100) / 100;
+    //     // console.log("this.length = " + this.length);
+    //     return avg;
+    // };
+    // this.toString = function(){
+    //     var str = "";
+    //     for(var key in this){
+    //         if(key !="toString" && key != "getAvg" && key != "getSum"){
+    //             str += key + "=" + this[key] + ", ";
+    //         }
+    //     }
+    //     str +=("총점=" + this.getSum() + ", 평점=" + this.getAvg());
+    //     return str;
+    // };
 }
-function test1(){
-    var students = [
-        new Student('김동현', 90, 56, 56, 45, 34),
-        new Student('홍길동', 45, 76, 34, 33, 56),
-        new Student('유관순', 76, 34, 54, 56, 22)
-    ];
-    for(var i = 0; i < students.length; i++){
-        console.log("" + students[i] + "총점=" + students[i].getSum() + ", 평점=" + students[i].getAvg());
+
+Student.prototype.getSum = function(){
+    return Number(this.java + this.oracle + this.html5 + this.css3 + this.javascript);
+};
+Student.prototype.getAvg = function(){
+    var avg = Math.round(this.getSum() / 5 * 100) / 100;
+    return avg;
+};
+Student.prototype.toString = function(){
+    var str = "";
+    for(var key in this){
+        if(key !="toString" && key != "getAvg" && key != "getSum"){
+            str += key + "=" + this[key] + ", ";
+        }
     }
+    str +=("총점=" + this.getSum() + ", 평점=" + this.getAvg());
+    return str;
+};
+
+function test1(){
+    // 1.객체배열 요소 추가
+    // var students = [
+    //     new Student('김동현', 90, 56, 56, 45, 34),
+    //     new Student('홍길동', 45, 76, 34, 33, 56),
+    //     new Student('유관순', 76, 34, 54, 56, 22)
+    // ];
+    var students = [];
+    students.push(new Student('김동현', 90, 56, 56, 45, 34));
+    students.push(new Student('홍길동', 45, 76, 34, 33, 56));
+    students.push(new Student('유관순', 76, 34, 54, 56, 22));
+
+    //2. 객체배열 출력
+    // for(var i = 0; i < students.length; i++){
+    //     // console.log("" + students[i] + "총점=" + students[i].getSum() + ", 평점=" + students[i].getAvg());
+    //     console.log("" + students[i]);
+    // }
+    students.forEach(function(student, i){
+        console.log(students.toString());
+    });
 }
